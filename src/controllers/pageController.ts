3f2082ebd03dc9dfaff7b2c9707dd1748b61cc4b
Alexandre Rosas <alexandre-rosas@hotmail.com>
2024-10-10 18:11:25 -0300

    criando views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home no controller!');
+    res.render('pages/page');
 }
 
 export const incluir = (req: Request, res: Response) => {
