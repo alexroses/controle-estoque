c796e91cef4742db9ed01827915b88521e05cc4f
alexroses <alexandrehmrosas@gmail.com>
2024-10-10 22:12:58 -0300

    Tentando criar a sessão

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import session from 'express-session';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
 import mainRoutes from './routes/index';
+import bodyParser from 'body-parser';
 
 
 dotenv.config();
@@ -12,6 +14,13 @@ const server = express();
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
+
+server.use(session({
+    secret: '1234',
+    resave: true,
+    saveUninitialized: true}));
+    
+server.use(bodyParser.urlencoded({extended:true}));
 
 server.use(express.static(path.join(__dirname, '../public')));
 
